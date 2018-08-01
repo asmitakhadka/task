b3b5363fd5333edac1f18b16e8fb05d8019a5368
Asmita Khadka <10015309@student.toiohomai.ac.nz>
2018-08-01 16:45:53 +1200

    todays ex

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("please input another number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, (num1 * num2));
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1 + num2));
+
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, (num1 - num2));
+
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, (num1 / num2));
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your first name");
+            string name1 = Console.ReadLine();
+
+            Console.WriteLine("please enter your last name");
+            string name2 = Console.ReadLine();
+            
+        }
+    }
+}
